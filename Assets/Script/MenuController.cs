ad0a8e06b7fb3013084b90ab4cb0c26f02bd86ef
Ewillian <guillaume.lecoq@ynov.com>
2021-04-29 11:08:19 +0200

    feat: animation gamemode & mainmenu

--- Assets/Script/MenuController.cs
+++ Assets/Script/MenuController.cs
@@ -1,30 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
 
-    public GameObject MainMenu;
+    private GameObject MainMenu;
 
-    public GameObject GamemodeMenu;
+    private GameObject GamemodeMenu;
 
-    public GameObject RuleMenu;
+    private GameObject RuleMenu;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        MainMenu = GameObject.FindGameObjectWithTag("Main Menu");
+        GamemodeMenu = GameObject.FindGameObjectWithTag("GameModeMenu");
+        RuleMenu = GameObject.FindGameObjectWithTag("Rule menu");
     }
 
-    // Update is called once per frame
-    void Update()
+   void ShowMainMenu()
     {
-        
+        MainMenu.GetComponent<Animator>().SetBool("Called", true);
     }
 
-    public void ButtonPlayOnClick()
+    public void ShowGamemode()
     {
+        MainMenu.GetComponent<Animator>().SetTrigger("EventOut");
+        GamemodeMenu.GetComponent<Animator>().SetTrigger("EventIn");
+    }
 
+    public void LeaveGamemode()
+    {
+        GamemodeMenu.GetComponent<Animator>().SetTrigger("EventOut");
+        MainMenu.GetComponent<Animator>().SetTrigger("EventIn");
+    }
+
+    public void LoadNoMoreCup()
+    {
+        SceneManager.LoadScene("BasicGamemodeScene");
     }
 }
